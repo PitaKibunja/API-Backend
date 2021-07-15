2bff31ead08167f059a4908ca7c79717f27d2411
dapintechnologies <dapintech@gmail.com>
2021-07-15 06:26:06 +0300

    serving api to frontend

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,9 +2,14 @@ exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
             {
+                _id:'1',
                 title:'First Post',
                 content:'This is the first Post',
-                imageUrl:'../images/bdd.jpg'
+                imageUrl:'../images/bdd.jpg',
+                creator:{
+                    name:'Pita'
+                },
+                createdAt:new Date()
             }
         ]
     })
@@ -17,9 +22,11 @@ exports.createPost = (req, res, next) => {
     res.status(201).json({
         message: 'Post created Successfully',
         post: {
-            id: new Date().toISOString(),
+            _id: new Date().toISOString(),
             title: title,
-            content: content
+            content: content,
+            creator:{name:'Pita'},
+            createdAt:new Date()
         }
     })
 }
